2f742aac3a3f5d0267a1edfab29e092f7ea1fddb
GlitchedLime <106375444+Gl1tch3dL1m3@users.noreply.github.com>
2026-02-09 02:20:49 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-const max = 0;
-const current = 0;
+var max = 0;
+var current = 0;
 
 window.SetStatusChanged = function(status)
 {
@@ -9,7 +9,7 @@ window.SetStatusChanged = function(status)
 window.SetFilesNeeded = function(needed)
 {
     current = max - needed
-    document.getElementById("loadingbar").style.width = current / max * 100;
+    document.getElementById("progress").style.width = current / max * 100 + "%";
 }
 
 window.SetFilesTotal = function(total)
