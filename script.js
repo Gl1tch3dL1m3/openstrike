7a73ce0e80fa730200fc5e19e5b430efec7bf129
GlitchedLime <106375444+Gl1tch3dL1m3@users.noreply.github.com>
2026-02-10 17:37:06 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 var max = 0;
-var current = 0;
 
 window.SetStatusChanged = function(status)
 {
@@ -8,11 +7,18 @@ window.SetStatusChanged = function(status)
 
 window.SetFilesNeeded = function(needed)
 {
-    current = max - needed
-    document.getElementById("progress").style.width = current / max * 100 + "%";
-}
+    if (max <= 0 || needed > max) max = needed;
+
+    var downloaded = max - needed;
+    var percentage = (downloaded / max) * 100;
+
+    if (percentage < 0) percentage = 0;
+    if (percentage > 100) percentage = 100;
+
+    document.getElementById("progress").style.width = percentage + "%";
+};
 
 window.SetFilesTotal = function(total)
 {
-    max = total
+    max = total;
 }
