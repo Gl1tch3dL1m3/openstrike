2bc30caa955d95d63d51319c40e4c92cc94ac5cc
GlitchedLime <106375444+Gl1tch3dL1m3@users.noreply.github.com>
2026-02-10 17:42:31 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,24 +1,24 @@
 var max = 0;
+var current = 0;
 
 window.SetStatusChanged = function(status)
 {
     document.getElementById("loadingtext").innerHTML = status;
 }
-
+/*
 window.SetFilesNeeded = function(needed)
 {
-    if (max <= 0 || needed > max) max = needed;
-
-    var downloaded = max - needed;
-    var percentage = (downloaded / max) * 100;
-
-    if (percentage < 0) percentage = 0;
-    if (percentage > 100) percentage = 100;
-
-    document.getElementById("progress").style.width = percentage + "%";
-};
+    current = max - needed;
+}
+*/
+window.DownloadingFile = function(file)
+{
+    document.getElementById("progress").style.width = current / max * 100 + "%";
+    current += 1;
+}
 
 window.SetFilesTotal = function(total)
 {
     max = total;
+    current = 0;
 }
